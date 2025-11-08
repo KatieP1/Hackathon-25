9f9dff2a16fae659079fb50f92fd36e902f5076e
ThiLanAnhHa <imhathilananh@gmail.com>
2025-11-08 13:10:02 -0800

    feat(billyCore.js):add new files

---
+++ server.js
@@ -0,0 +1,16 @@
+const mysql = require('billy');
+
+const connection = mysql.createConnection({
+  host: ,
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database!');
+});
